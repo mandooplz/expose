ea01eafb0a39c95b3c63b3c1fd24cb5b0642268a
kmw4561@gmail.com <kmw4561@gmail.com>
2026-01-03 03:04:36 +0900

    [chore] 매크로 프로토콜 채택 업데이트

--- Sources/Expose/Exposable/Exposable.swift
+++ Sources/Expose/Exposable/Exposable.swift
@@ -5,8 +5,9 @@
 //  Created by 김민우 on 1/2/26.
 //
 import Foundation
+import Combine
 
 
 @attached(member, names: named(registrar))
-@attached(extension, conformances: ExposableObject)
+@attached(extension, conformances: NewExposableObject, Combine.ObservableObject)
 public macro Exposable() = #externalMacro(module: "ExposeMacros", type: "ExposableMacro")
