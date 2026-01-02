a94174aa77fffc62bbbd019de3b549a095b54916
kmw4561@gmail.com <kmw4561@gmail.com>
2026-01-03 03:17:46 +0900

    [chore] ObservableObject 프로토콜 준수 추가

--- Sources/Expose/Exposed/ExposableObject.swift
+++ Sources/Expose/Exposed/ExposableObject.swift
@@ -7,9 +7,10 @@
 
 
 @_exported import Observation
+@_exported import Combine
 
 
 @available(iOS 17.0, *)
-public protocol ExposableObject: AnyObject, Observation.Observable {
+public protocol ExposableObject: AnyObject, Observation.Observable, ObservableObject {
     var registrar: ObservationRegistrar { get }
 }
